da7cbf23b43110250be8b6d7bcc68ebf88d80d88
Mirco Deb <mirco.meaz@gmail.com>
2019-02-12 18:40:25 +0100

    Extended to all variables

--- fft_support.c
+++ fft_support.c
@@ -14,185 +14,6 @@
 typedef double FFT_SCALAR;
 
 /*============================================= Functions Def =============================================*/
-
-void print_array( double *work, int insize, int elem_per_proc, int rank, char string[100] ) {
-
-	//Print work row-wise
-	printf("\n\n%s\n"
-			"LOCAL array on rank: %d\n", string, rank);
-	double re, im;
-	int i = 0;
-	while ( i < insize*2 ){
-		re = work[i];
-		i++;
-		im = work[i];
-		i++;
-		printf("%f+i(%f)\t\t", re, im);
-		if ( i % (elem_per_proc*2) == 0){
-			printf("\n=============\n");
-		}
-	}
-}
-
-void FFT( double *c, int N, int isign ) {
-	/**********************************************************************
-	  FFT - calculates the discrete fourier transform of an array of double
-	  precision complex numbers using the FFT algorithm.
-
-	  c = pointer to an array of size 2*N that contains the real and
-	    imaginary parts of the complex numbers. The even numbered indices contain
-	    the real parts and the odd numbered indices contain the imaginary parts.
-	      c[2*k] = real part of kth data point.
-	      c[2*k+1] = imaginary part of kth data point.
-	  N = number of data points. The array, c, should contain 2*N elements
-	  isign = 1 for forward FFT, -1 for inverse FFT.
-
-	  Copyright (C) 2003, 2004, 2005 Exstrom Laboratories LLC
-	*/
-  int n, n2, nb, j, k, i0, i1;
-  double wr, wi, wrk, wik;
-  double d, dr, di, d0r, d0i, d1r, d1i;
-  double *cp;
-
-  j = 0;
-  n2 = N / 2;
-  for( k = 0; k < N; ++k )
-  {
-    if( k < j )
-    {
-      i0 = k << 1;
-      i1 = j << 1;
-      dr = c[i0];
-      di = c[i0+1];
-      c[i0] = c[i1];
-      c[i0+1] = c[i1+1];
-      c[i1] = dr;
-      c[i1+1] = di;
-    }
-    n = N >> 1;
-    while( (n >= 2) && (j >= n) )
-    {
-      j -= n;
-	  n = n >> 1;
-    }
-    j += n;
-  }
-
-  for( n = 2; n <= N; n = n << 1 )
-  {
-    wr = cos( 2.0 * M_PI / n );
-    wi = sin( 2.0 * M_PI / n );
-    if( isign == 1 ) wi = -wi;
-    cp = c;
-    nb = N / n;
-    n2 = n >> 1;
-    for( j = 0; j < nb; ++j )
-    {
-      wrk = 1.0;
-      wik = 0.0;
-      for( k = 0; k < n2; ++k )
-      {
-        i0 = k << 1;
-        i1 = i0 + n;
-        d0r = cp[i0];
-        d0i = cp[i0+1];
-        d1r = cp[i1];
-        d1i = cp[i1+1];
-        dr = wrk * d1r - wik * d1i;
-        di = wrk * d1i + wik * d1r;
-        cp[i0] = d0r + dr;
-        cp[i0+1] = d0i + di;
-        cp[i1] = d0r - dr;
-        cp[i1+1] = d0i - di;
-        d = wrk;
-        wrk = wr * wrk - wi * wik;
-        wik = wr * wik + wi * d;
-      }
-      cp += n << 1;
-    }
-  }
-}
-
-void b_FFT( double *work, int elem_per_proc, int N_trasf) {
-
-	double* in = (double *) malloc( sizeof(double)*2*N_trasf );
-	int count = 0;
-	while ( count < elem_per_proc/(2*N_trasf)) {			// To move among rows in the pencil
-		// fill IN array
-		for ( int i = 0; i < 2*N_trasf; i++ ){
-			in[i] = work[i+count*2*N_trasf];
-		}
-		// Execute FFT & Normalize
-		FFT( in, N_trasf, -1 );
-		for (int i = 0; i < 2*N_trasf; i++) {
-			work[i+count*2*N_trasf] = in[i] / (N_trasf);
-		}
-		count++;
-	}
-	free(in);
-}
-
-void f_FFT( double *work, int elem_per_proc, int N_trasf) {
-
-	double* in = (double *) malloc( sizeof(double)*2*N_trasf );
-	int count = 0;
-	while ( count < elem_per_proc/(2*N_trasf)) {			// To move among rows in the pencil
-		// fill IN array
-		for ( int i = 0; i < 2*N_trasf; i++ ){
-			in[i] = work[i+count*2*N_trasf];
-		}
-		// Execute FFT
-		FFT( in, N_trasf, +1 );
-		for (int i = 0; i < 2*N_trasf; i++) {
-			work[i+count*2*N_trasf] = in[i];
-		}
-		count++;
-	}
-	free(in);
-}
-
-void check_results( double *work, double *work_ref, int elem_per_proc) {
-
-	/* Remind to add in "Memory Filling" section the following lines od code:
-	  FFT_SCALAR *u_ref = (FFT_SCALAR *) malloc(remapsize*sizeof(FFT_SCALAR)*2);
-  	  FFT_SCALAR *v_ref = (FFT_SCALAR *) malloc(remapsize*sizeof(FFT_SCALAR)*2);
-  	  FFT_SCALAR *w_ref = (FFT_SCALAR *) malloc(remapsize*sizeof(FFT_SCALAR)*2);
-  	  MPI_Scatter( U, elem_per_proc , MPI_DOUBLE, u_ref, elem_per_proc,  MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  	  MPI_Scatter( V, elem_per_proc , MPI_DOUBLE, v_ref, elem_per_proc,  MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  	  MPI_Scatter( W, elem_per_proc , MPI_DOUBLE, w_ref, elem_per_proc,  MPI_DOUBLE, 0, MPI_COMM_WORLD);
- */
-
-	  double mostdiff = 0.0, mydiff= 0.0;
-	  for ( int i = 0; i < elem_per_proc; i++) {
-		  mydiff = fabs( work[i] - work_ref[i]);
-		  if ( mydiff > mostdiff ) {
-			  mostdiff = mydiff;
-			  printf("Max difference in initial/final values = %.20f\n",mostdiff);
-	  	  }
-		}
-	  //print_array( work_ref, insize, N_trasf, rank, "Reference");
-}
-
-void generate_inputs(FFT_SCALAR *U, FFT_SCALAR *V, FFT_SCALAR *W, int nfast, int nmid, int nslow, int rank) {
-	//Generate Input
-	//int q= 0.0;
-	if (rank == 0){
-	  for ( int i = 0; i < nslow*nmid*nfast *2; i++) {
-		  //U[i] = q++;
-		  U[i] = (float)rand()/ (float)(RAND_MAX/10);
-	  }
-	  for ( int i = 0; i < nslow*nmid*nfast *2; i++) {
-		  //V[i] = q++;
-		  V[i] = (float)rand()/ (float)(RAND_MAX/10);
-	  }
-	  for ( int i = 0; i < nslow*nmid*nfast *2; i++) {
-		  //W[i] = q++;
-		  W[i] = (float)rand()/ (float)(RAND_MAX/10);
-	  }
-  }
-
-}
-
 void z_aliasing(int nx, int ny, int nz, int nzd, FFT_SCALAR *U, FFT_SCALAR *U_read){
 		int nz_left = 1+ (nz-1)/2; 	int reader=0;
 	
@@ -248,8 +69,6 @@ void x_dealiasing(int scounts, int nx, int nxd, FFT_SCALAR *u) {
 	}
 }
 
-
-
 void z_dealiasing(int nx, int ny, int nz, int nzd, FFT_SCALAR *U) {
 	int writer=0;	int reader=0;	int nz_left = 1+(nz-1)/2;
 	FFT_SCALAR *temp = (FFT_SCALAR *) malloc(2*nz_left*sizeof(FFT_SCALAR));
@@ -277,43 +96,6 @@ void z_dealiasing(int nx, int ny, int nz, int nzd, FFT_SCALAR *U) {
 	}
 }
 
-void transpose_on_rank0(int nx, int ny, int nz, FFT_SCALAR *U) {
-	/* This routine remap the array in such a manner that negative modes appears on the left side
-	 * of the array. Once remap is completed, the array is transposed in y pencil */
-	struct cmplx {
-		double re, im;
-	};
-
-	int reader = 0, writer = 0;
-	// Fill the array on rank 0
-	struct cmplx u_mat[nx][ny];
-	for (int k = 0; k < nz; k++) {
-
-		// Read the k-th plane
-		for (int j = 0; j < ny; j++) {
-			for (int i = 0; i < nx; i++) {
-				//printf("U[%d] = %g\n", reader, U[reader]);
-				u_mat[i][j].re = U[reader];
-				reader++;
-				//printf("U[%d] = %g\n", reader, U[reader]);
-				u_mat[i][j].im = U[reader];
-				reader++;
-			}
-		}
-		// Transpose the k-th plane
-		for (int i = 0; i < nx; i++) {
-			for (int j = 0; j < ny; j++) {
-				U[writer] = u_mat[i][j].re;
-				//printf("U[%d] = %g\n", writer, U[writer]);
-				writer++;
-				U[writer] = u_mat[i][j].im;
-				//printf("U[%d] = %g\n", writer, U[writer]);
-				writer++;
-			}
-		}
-	}
-}
-
 void cores_handler( int modes, int size, int *modes_per_proc) {
 	int rank =0;
 	int check=0;
@@ -331,17 +113,17 @@ void cores_handler( int modes, int size, int *modes_per_proc) {
 	if ( (int)(check - modes) != 0 ) {
 			printf("[ERROR] check - modes = %d!!\nUnable to scatter modes properly\nAbort... \n", check - modes);
 	}
-
-
 }
 
 void read_data(int nx, int ny, int nz, FFT_SCALAR *U_read, char file_to_read[4]) {
 	//On rank 0 read the dataset
 	FILE *U_dat;	U_dat = fopen( file_to_read, "r");
+	printf("Reading initial field from %s...\n",file_to_read);
 	for ( int i = 0; i < (nx)*(ny)*(nz)*2; i++) {
 		fscanf( U_dat, "%lf", &U_read[i]);
 		//printf("I've read %lf\n", U_read[i]);
 	}
+	printf("\tReading completed\n");
 }
 
 /* APPLY GLOBAL AA Z PENCIL VERSION */
@@ -442,25 +224,24 @@ if (rank == desidered_rank) {
 }
 }
 
-void print_y_pencil(int nx, int ny, int nz, FFT_SCALAR *u, int rank,
-		int displs, int scounts, int desidered_rank) {
+void print_y_pencil(int ny, int y_ilo, int y_khi, int y_klo, 
+		FFT_SCALAR *u, int rank, int scounts, int desidered_rank) {
 if (rank == desidered_rank) {
-	  int total_modes = displs/ (ny*2);
-	  int stride_nz = total_modes / nx;
-	  int stride_nx = total_modes - stride_nz * nx;
+	int stride_nx = y_ilo;
+	int stride_nz = y_klo;
 
-	  for (int i = 0; i < scounts; i++) {
-   	  if ( i % (ny*2) == 0) {
-   		  printf("========(nx= %d, nz= %d)=======\n", stride_nx , stride_nz);
-   		  if ( (stride_nx ) == (nx-1)) {
-   			  stride_nx =0;
-   			  stride_nz ++;
-   		  }
-   		  else stride_nx++;
-   	  }
-   	 printf("u[%d]= %.10f\n", (i), u[i]);
-     }
- }
+	for (int i = 0; i < scounts; i++) {
+   		if ( i % (ny*2) == 0) {
+   			printf("========(nx= %d, nz= %d)=======\n", stride_nx , stride_nz);
+   			if ( (stride_nz ) == y_khi) {
+   				stride_nz = y_klo;
+   				stride_nx++;
+   			}
+   			else stride_nz++;
+   			}
+   		printf("u[%d]= %.10f\n", (i), u[i]);
+    	}
+ 	}
 }
 
 void print_z_pencil(int nz, int in_ilo, int in_ihi, int in_jlo,
--- fft_support.h
+++ fft_support.h
@@ -8,29 +8,20 @@
 #ifndef FFT_SUPPORT_H_
 #define FFT_SUPPORT_H_
 
-
-void print_array( double *work, int insize, int elem_per_proc, int rank, char string[100] );
 void print_x_pencil(int nx, int in_jlo, int in_jhi, int in_klo,
 		FFT_SCALAR *u, int rank, int scounts, int desidered_rank);
-void print_y_pencil(int nx, int ny, int nz, FFT_SCALAR *u, int rank,
-		int displs, int scounts, int desidered_rank);
+void print_y_pencil(int ny, int y_ilo, int y_ihi, int y_klo, 
+		FFT_SCALAR *u, int rank, int scounts, int desidered_rank);
 void print_z_pencil(int nz, int in_ilo, int in_ihi, int in_jlo,
 		FFT_SCALAR *u, int rank, int scounts, int desidered_rank);
-void FFT( double *c, int N, int isign );
-void b_FFT( double *work, int elem_per_proc, int N_trasf);
-void f_FFT( double *work, int elem_per_proc, int N_trasf);
 void read_data(int nx, int ny, int nz, FFT_SCALAR *U_read, char file_to_read[4]);
 void apply_AA(int nx, int ny, int nz, int nxd, int nzd, FFT_SCALAR *U, FFT_SCALAR *U_read);
 void z_aliasing(int nx, int ny, int nz, int nzd, FFT_SCALAR *U, FFT_SCALAR *U_read);
 void x_aliasing(int nx, int ny, int nzd,int nxd, FFT_SCALAR *U, FFT_SCALAR *U_read);
 void x_dealiasing(int scounts, int nx, int nxd, FFT_SCALAR *u);
 void z_dealiasing(int nx, int ny, int nz, int nzd, FFT_SCALAR *U);
-void transpose_on_rank0(int nx, int ny, int nz, FFT_SCALAR *U);
 void cores_handler( int modes, int size, int *modes_per_proc);
 void Alltoall(int rank, int size, int in_jlo, int in_jhi, int in_ilo,
 					 int in_ihi, int nz, int ny, FFT_SCALAR *arr, FFT_SCALAR *arr_recv, int flag);
-// No longer in use
-void check_results( double *work, double *work_ref, int elem_per_proc);
-void generate_inputs(FFT_SCALAR *U, FFT_SCALAR *V, FFT_SCALAR *W, int nfast, int nmid, int nslow, int rank) ;
 
 #endif /* FFT_SUPPORT_H_ */
